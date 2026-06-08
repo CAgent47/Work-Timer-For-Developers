74398e5ef5390e82a4aba1f2c938a5fd232808fd
47 <mpgbieghesh468@gmail.com>
2026-06-08 12:03:54 +0330

    working_time.py

--- working_timer/working_time.py
+++ working_timer/working_time.py
@@ -10,8 +10,6 @@ def working(minutes):
     """
     seconds = minutes * 60
     while seconds > 0:
-        print(f"Time remaining: {seconds // 60:02d}:{seconds % 60:02d}")
         time.sleep(1)
         seconds -= 1
-    print("Time's up!")
     return seconds
